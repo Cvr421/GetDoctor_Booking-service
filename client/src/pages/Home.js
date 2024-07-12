f5b4a745e08dbdd4c7c071c4a6b25d51d2a9929f
Cvr421 <chandravijayk42187@gmail.com>
2024-07-12 20:14:33 +0530

    Home page completed

---
+++ client/src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Contact from "../component/Contact";
+import AboutUs from "../component/AboutUs";
+import Footer from "../component/Footer";
+import Hero from "../component/Hero";
+import Navbar from "../component/Navbar";
+import HomeCircles from "../component/HomeCircles";
+
+export function Home(){
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <HomeCircles />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
