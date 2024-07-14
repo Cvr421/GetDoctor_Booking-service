374e78caf7caaca06d26414ead5d494707d3e5a2
Cvr421 <chandravijayk42187@gmail.com>
2024-07-14 22:49:10 +0530

    Updates

---
+++ static/js/952.0aefd27d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[952],{952:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(43);var c=r(475),n=r(579);const t=()=>(0,n.jsxs)("div",{className:"error container",children:[(0,n.jsx)("h2",{children:"Error! Page Not Found"}),(0,n.jsx)(c.k2,{to:"/",className:"btn",children:"go to home"})]})}}]);
+//# sourceMappingURL=952.0aefd27d.chunk.js.map
