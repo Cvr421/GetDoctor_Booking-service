10b02bc01bcced820de8fdc63670ecfef7460766
Cvr421 <chandravijayk42187@gmail.com>
2024-07-14 22:30:09 +0530

    setting up production Deployment

---
+++ client/build/static/js/423.f96779e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[423],{423:(t,e,a)=>{a.d(e,{WQq:()=>i});var c=a(441);function i(t){return(0,c.k5)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M405 136.798L375.202 107 256 226.202 136.798 107 107 136.798 226.202 256 107 375.202 136.798 405 256 285.798 375.202 405 405 375.202 285.798 256z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=423.f96779e9.chunk.js.map
